1d2f021e9dd4a7a778ec77f80635ab486e0676f1
Louna Vizio <nalou0123@gmail.com>
2021-02-11 14:40:16 +0100

    add folders

--- README.md
+++ README.md
@@ -5,8 +5,44 @@
 * Programmé en Python
 * Utilisant la bibliothèque pygame
 * Votre code est hébergé sur GitHub
-* Vous travaillez en team de 2 personne 
+* Vous travaillez en team de 2 personne (Sarah Marcoz 3M5) 
 * Votre programme est interactif (souris, clavier)
 * Votre programme utilise des média (images, sons)
 * Vous documentez votre programme (en Markup)
 
+
+# oc20 -Projet Editor 
+
+## Introduction
+#### Rendu le 2 mars 2020 
+
+- Editeur graphique 
+- Permet de manipuler des objets (rectangles, cercles, lignes, images)
+- Permet de choisir couleurs et épaisseurs 
+- Permet de créer, déplacer, supprimer des objets 
+- Permet de sauvegarder/charger un dessin
+
+
+## Description 
+Décrivez ce que votre éditeur peut faire 
+
+## Interfaces 
+Capture d'écran 
+
+
+
+## Raccourcis claviers 
+
+- R pour insérer un rectangle 
+- L pour insérer une ligne 
+- C pour insérer un cercle 
+
+Pour changement de 
+- alt+1 pour ligne d'épaisseur 1 
+- alt+0 pout figure remplie 
+- alt+2 pour ligne d'épaisseur 2 
+
+
+## Manipulation avec la souris 
+
+## Conclusion 
--- tutorial /lesson 3/rect5:2.py .py
+++ tutorial /lesson 3/rect5:2.py .py
@@ -1,21 +1,49 @@
 from rect import *
 
-rect0 = Rect(50, 60, 200, 80)
-rect = rect0.copy()
+r0 = Rect(50, 60, 200, 80)
+r1 = Rect(100, 20, 100, 140)
 
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-        
+
         if event.type == KEYDOWN:
             if event.key in dir:
-                v = dir[event.key]
-                rect.inflate_ip(v)
+                r1.move_ip(dir[event.key])
 
+    clip = r0.clip(r1)
+    union = r0.union(r1)
+    
     screen.fill(GRAY)
-    pygame.draw.rect(screen, BLUE, rect0, 1)
-    pygame.draw.rect(screen, RED, rect, 4)
+    pygame.draw.rect(screen, YELLOW, union, 0)
+    pygame.draw.rect(screen, GREEN, clip, 0)
+    pygame.draw.rect(screen, BLUE, r0, 4)
+    pygame.draw.rect(screen, RED, r1, 4)
+    pygame.display.flip()
+
+pygame.quit()from rect import *
+
+r0 = Rect(50, 60, 200, 80)
+r1 = Rect(100, 20, 100, 140)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+        if event.type == KEYDOWN:
+            if event.key in dir:
+                r1.move_ip(dir[event.key])
+
+    clip = r0.clip(r1)
+    union = r0.union(r1)
+    
+    screen.fill(GRAY)
+    pygame.draw.rect(screen, YELLOW, union, 0)
+    pygame.draw.rect(screen, GREEN, clip, 0)
+    pygame.draw.rect(screen, BLUE, r0, 4)
+    pygame.draw.rect(screen, RED, r1, 4)
     pygame.display.flip()
 
 pygame.quit()
