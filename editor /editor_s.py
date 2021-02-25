3e8f60dd149d29d593ba62f4297794ff5f4cec49
sarah0502 <sarah.marcoz.2003@gmail.com>
2021-02-25 13:57:01 +0100

    add

--- editor /editor_s.py
+++ editor /editor_s.py
@@ -1,2 +1,29 @@
 import pygame
 from pygame.locals import *
+
+SIZE = 1500, 1500
+RED = (255, 0, 0)
+GRAY = (150, 150, 150)
+
+pygame.init()
+screen = pygame.display.set_mode(SIZE)
+
+img = pygame.image.load('mariopaysage.jpg')
+img.convert()
+
+rect = img.get_rect()
+rect.center = 600, 20
+moving = False
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+    screen.fill(GRAY)
+    pygame.draw.rect(screen, RED, rect, 2)
+    screen.blit(img, rect)
+    pygame.display.flip()
+
+pygame.quit()
