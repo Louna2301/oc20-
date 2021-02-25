c95366e22e2ff2afde160213dbf71bf2fdb8ff8d
sarah0502 <sarah.marcoz.2003@gmail.com>
2021-02-25 14:52:37 +0100

    Update editor_S_2.py

--- editor /editor_S_2.py
+++ editor /editor_S_2.py
@@ -10,9 +10,18 @@ screen = pygame.display.set_mode(SIZE)
 
 img = pygame.image.load('mariosurpris.jpg')
 img.convert()
+img = pygame.transform.scale(img, (300, 300))
+img_2 = pygame.image.load('Boser.png')
+img_2.convert()
+img_2 = pygame.transform.scale(img_2, (400, 400))
 
 rect = img.get_rect()
-rect.center = 300, 500
+rect.center = 400, 500
+moving = False
+running = True
+
+rect_2 = img_2.get_rect()
+rect_2.center = 1000, 500
 moving = False
 running = True
 
@@ -24,6 +33,7 @@ while running:
     screen.fill(GRAY)
     pygame.draw.rect(screen, RED, rect, 2)
     screen.blit(img, rect)
+    screen.blit(img_2, rect_2)
     pygame.display.flip()
 
 pygame.quit()
