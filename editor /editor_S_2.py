8371714bdc16230f28d891be5ac136809d3a9f56
sarah0502 <sarah.marcoz.2003@gmail.com>
2021-02-25 14:23:12 +0100

    Update editor_S_2.py

--- editor /editor_S_2.py
+++ editor /editor_S_2.py
@@ -14,21 +14,16 @@ img.convert()
 rect = img.get_rect()
 rect.center = 600, 20
 moving = False
-
-class Shape:
-    def __init__(self, rect, type_ = 'r'):
-        self.rect = img
-        self.type_ = type_
-        
-    def draw(self):
-        if self.type == 'r':
-            pygame.draw.rect(screen, self.rect)
-        
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-            if event.key == K_e:
-                type_ = 'e'
+
+    screen.fill(GRAY)
+    pygame.draw.rect(screen, RED, rect, 2)
+    screen.blit(img, rect)
+    pygame.display.flip()
+
+pygame.quit()
