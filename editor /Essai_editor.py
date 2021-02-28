1a46545c6921f09027763eee2fff76b862f41fa8
sarah0502 <sarah.marcoz.2003@gmail.com>
2021-02-28 16:48:13 +0100

    Update Essai_editor.py

--- editor /Essai_editor.py
+++ editor /Essai_editor.py
@@ -29,7 +29,12 @@ print(key_dict)
 pygame.init()
 
 # Créer une nouvelle fenêtre
-screen=pygame.display.set_mode((640, 480))
+SIZE = 1500, 1500
+RED = (255, 0, 0)
+GRAY = (150, 150, 150)
+
+pygame.init()
+screen = pygame.display.set_mode(SIZE)
 screen.fill(GREEN)
 pygame.display.update() # est nécessaire pour afficher les changements
 
@@ -53,6 +58,33 @@ while running:
             pygame.display.set_caption(caption)
                 
             screen.fill(background)
-            pygame.display.update()
+
+img = pygame.image.load('mariosurpris.jpg')
+img.convert()
+img = pygame.transform.scale(img, (300, 300))
+img_2 = pygame.image.load('Boser.png')
+img_2.convert()
+img_2 = pygame.transform.scale(img_2, (400, 400))
+
+rect = img.get_rect()
+rect.center = 400, 500
+moving = False
+running = True
+
+rect_2 = img_2.get_rect()
+rect_2.center = 1000, 500
+moving = False
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+    screen.fill(GRAY)
+    pygame.draw.rect(screen, RED, rect, 2)
+    screen.blit(img, rect)
+    screen.blit(img_2, rect_2)
+    pygame.display.flip()
         
 pygame.quit()
