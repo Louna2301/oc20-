ccc3540655d86f0bfe931b620fa36cf9aedb4a6b
sarah0502 <sarah.marcoz.2003@gmail.com>
2021-03-04 13:54:23 +0100

    Update Essai_fond.py

--- editor /Essai_fond.py
+++ editor /Essai_fond.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import time
 
 
 #Tools > install pygame
@@ -79,5 +80,44 @@ while running:
     screen.blit(img, rect)
     screen.blit(img_2, rect_2)
     pygame.display.flip()   
-        
+
+sysfont = pygame.font.get_default_font()
+print('system font :', sysfont)
+font = pygame.font.SysFont(None, 24)
+img = font.render('hello', True, BLUE)
+screen.blit(img, (20, 20))
+t0 = time.time()
+font = pygame.font.SysFont(None, 48)
+print('time needed for Font creation :', time.time()-t0)
+fonts = pygame.font.get_fonts()
+print(len(fonts))
+for f in fonts:
+    print(f)
+img = font.render(sysfont, True, RED)
+rect = img.get_rect()
+pygame.draw.rect(img, BLUE, rect, 1)
+font1 = pygame.font.SysFont('chalkduster.ttf', 72)
+img1 = font1.render('chalkduster.ttf', True, BLUE)
+
+font2 = pygame.font.SysFont('didot.ttc', 72)
+img2 = font2.render('didot.ttc', True, GREEN)
+fonts = pygame.font.get_fonts()
+print(len(fonts))
+for i in range(7):
+    print(fonts[i])
+
+    running = True
+    background = GRAY
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                 running = False
+
+            screen.fill(background)
+            screen.blit(img, (20, 20))
+            screen.blit(img1, (20, 50))
+            screen.blit(img2, (20, 120))
+            pygame.display.update()
+
+
 pygame.quit()
