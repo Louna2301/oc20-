f88ec662e6c212f04de1d0ca3bbb29c5c9042bcb
sarah0502 <sarah.marcoz.2003@gmail.com>
2021-03-13 13:14:30 +0100

    Update Editor.py

--- editor /Editor.py
+++ editor /Editor.py
@@ -110,8 +110,49 @@ while running:
     pygame.display.update()
 
 pygame.quit()
+
 #Nouvelle fenêtre
+pygame.init()
 SIZE = 1500, 1500
 screen = pygame.display.set_mode(SIZE)
 screen.fill(GRAY)
 pygame.display.update() # est nécessaire pour afficher les changements
+
+import pygame
+from pygame.locals import *
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+GRAY = (150, 150, 150)
+
+drawing = False
+points = []
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                if len(points) > 0:
+                    points.pop()
+
+        elif event.type == MOUSEBUTTONDOWN:
+            points.append(event.pos)
+            drawing = True
+
+        elif event.type == MOUSEBUTTONUP:
+            drawing = False
+
+        elif event.type == MOUSEMOTION and drawing:
+            points[-1] = event.pos
+    
+    screen.fill(GRAY)
+    if len(points)>1:
+        rect = pygame.draw.lines(screen, RED, True, points, 3)
+        pygame.draw.rect(screen, GREEN, rect, 1)
+    pygame.display.update()
+
+pygame.quit()
