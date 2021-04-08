fe5329abf6bb350c9eb62a1e4bffa14a9441b228
sarah0502 <sarah.marcoz.2003@gmail.com>
2021-04-08 17:44:18 +0200

    Update Jeu.py

--- game /Jeu.py
+++ game /Jeu.py
@@ -42,14 +42,27 @@ class Projectile(pygame.sprite.Sprite):
     def __init__(self, player):
         super().__init__()
         self.velocity = 3
+        self.player = player
         self.image = pygame.image.load('image/carapace.png')
         self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect()
         self.rect.x = player.rect.x + 205 
         self.rect.y = player.rect.y + 80
+        self.origine_image = self.image
+        self.angle = 0
+        
+    def rotate(self):
+        self.angle += 12
+        
+    def remove(self):
+        self.player.all_projectiles.remove(self)
         
     def move(self):
         self.rect.x += self.velocity
+        
+        if self.rect.x > 1080:
+            #supprimer le projectile sorti de l'ecran
+            self.remove()
             
 # fenetre du jeu
 pygame.display.set_caption('game') 
