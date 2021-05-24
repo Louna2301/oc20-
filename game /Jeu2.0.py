e99961e961b8be88c7196a625ac9d98f7cdad2a3
sarah0502 <sarah.marcoz.2003@gmail.com>
2021-05-24 11:51:52 +0200

    Update Jeu2.0.py

--- game /Jeu2.0.py
+++ game /Jeu2.0.py
@@ -7,7 +7,26 @@ class Game:
     def __init__(self):
         # definir si notre jeu a commencé
         self.is_playing = False
-
+        
+    def start(self):
+        self.is_playing = True
+        
+# classe palet
+class Palet(pygame.sprite.Sprite):
+    
+    def __init__(self, game):
+        super().__init__()
+        self.game = game
+        self.rect.x = 100
+        self.rect.y = 525
+        
+    def move_right(self):
+        if not self.game.check_collision(self, self.game.all_monsters):
+            self.rect.x += self.velocity
+        
+    def move_left(self):
+        self.rect.x -= self.velocity
+            
 # fenetre du jeu
 pygame.display.set_caption('Pong') 
 screen = pygame.display.set_mode((1080, 720))
