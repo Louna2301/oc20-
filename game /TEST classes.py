9a7ba160c6b0a79475a98290fcec9b95ac24c704
sarah0502 <sarah.marcoz.2003@gmail.com>
2021-05-24 20:14:41 +0200

    add test classes

--- game /TEST classes.py
+++ game /TEST classes.py
@@ -1,20 +1,15 @@
-import pygame
-pygame.init()
-
 # classe jeu
 class Game:
-    
     def __init__(self):
         # definir si notre jeu a commencé
         self.is_playing = False
         
     def start(self):
         self.is_playing = True
-        
+
 # classe palet
 class Palet(pygame.sprite.Sprite):
-    
-    def __init__(self, game):
+     def __init__(self, game):
         super().__init__()
         self.game = game
         self.rect.x = 100
@@ -26,12 +21,11 @@ class Palet(pygame.sprite.Sprite):
         
     def move_left(self):
         self.rect.x -= self.velocity
-            
-# fenetre du jeu
-pygame.display.set_caption('Pong') 
-screen = pygame.display.set_mode((1080, 720))
 
-# arrière plan du jeu
-background = pygame.image.load('images2.0/background.png')
-
-pygame.quit()
+# classe balle
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, game):
+        super().__init__()
+        self.game = game
+        self.rect.x = 100
+        self.rect.y = 525
